4aa292b2ceca0b9587becc2df4fb09134630f667
Joe Wilson <joe.wilson@jwils.xyz>
2025-04-18 19:15:06 -0400

    Backend validation and improved logging

--- src/api/search-emoji.ts
+++ src/api/search-emoji.ts
@@ -4,29 +4,46 @@ import { Groq } from 'groq-sdk';
 
 const groq = new Groq();
 export async function searchEmoji(query: string): Promise<string[]> {
-  const response = await groq.chat.completions.create({
-    messages: [
-      {
-        role: 'system',
-        content:
-          "Provide 5-10 relevant emojis based on the main idea of the user's message. Do not respond with anything besides the emojis",
-      },
-      {
-        role: 'user',
-        content: query,
-      },
-    ],
-    model: 'llama3-8b-8192',
-    temperature: 1,
-    max_completion_tokens: 128,
-    top_p: 1,
-    stream: false,
-    stop: null,
-  });
+  if (query.length > 50) {
+    console.error('Query too long');
+    throw 'Query too long';
+  }
 
-  const message = response.choices[0].message.content;
+  const safeQuery = query.trim().replace(/[^a-zA-Z0-9\s]/g, '');
 
-  console.log({ input: query, output: message });
+  if (!safeQuery) {
+    console.error('Empty query');
+    throw 'Empty query';
+  }
+
+  try {
+    const response = await groq.chat.completions.create({
+      messages: [
+        {
+          role: 'system',
+          content:
+            "Provide 5-10 relevant emojis based on the main idea of the user's message. Do not respond with anything besides the emojis",
+        },
+        {
+          role: 'user',
+          content: safeQuery,
+        },
+      ],
+      model: 'llama3-8b-8192',
+      temperature: 1,
+      max_completion_tokens: 128,
+      top_p: 1,
+      stream: false,
+      stop: null,
+    });
+
+    var message = response.choices[0].message.content;
+  } catch (error) {
+    console.error('Error query', { error, query, input: safeQuery });
+    throw 'Error communicating with LLM';
+  }
+
+  console.log('Successful query', { query, input: safeQuery, output: message });
 
   return message ? extractEmojis(message) : [];
 }
