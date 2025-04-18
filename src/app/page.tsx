aaa0fea19425d7061b6751ad886151a39e915b11
Joe Wilson <joe.wilson@jwils.xyz>
2025-04-18 19:35:24 -0400

    Add frontend loading/error states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,18 @@ import SearchBar from '@/components/SearchBar';
 import Results from '@/components/Results';
 import { searchEmoji } from '@/api/search-emoji';
 import { copyToClipboard } from '@/lib/client/copy-to-clipboard';
+import LoadingSpinner from '@/components/LoadingSpinner';
+import { Alert, AlertTitle, AlertDescription } from '@/components/ui/alert';
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState<string[]>([]);
-  const [hasSearched, setHasSearched] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
 
   const validateQuery = () => {
     if (!searchQuery.trim()) {
       setSearchResults([]);
-      setHasSearched(false);
       return false;
     }
     return true;
@@ -24,23 +26,29 @@ export default function Home() {
   const handleSearch = async () => {
     if (!validateQuery()) return;
 
-    const results = await searchEmoji(searchQuery);
+    setIsFetching(true);
+    const results = await searchEmoji(searchQuery)
+      .catch(() => setIsError(true))
+      .finally(() => setIsFetching(false));
+    if (!results) return;
 
     setSearchResults(results);
-    setHasSearched(true);
   };
 
   const handlePickRandom = async () => {
     if (!validateQuery()) return;
 
-    const results = await searchEmoji(searchQuery);
+    setIsFetching(true);
+    const results = await searchEmoji(searchQuery)
+      .catch(() => setIsError(true))
+      .finally(() => setIsFetching(false));
+    if (!results) return;
 
     const randomIndex = Math.floor(Math.random() * results.length);
     const randomEmoji = results[randomIndex];
     copyToClipboard(randomEmoji);
 
     setSearchResults([randomEmoji]);
-    setHasSearched(true);
   };
 
   const handleKeyDown = (e: React.KeyboardEvent) => {
@@ -53,14 +61,27 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gradient-to-b from-yellow-100 to-yellow-200 dark:from-gray-900 dark:to-gray-800">
       <div className="w-full max-w-md mx-auto text-center space-y-8">
         <Header />
-        <SearchBar
-          searchQuery={searchQuery}
-          setSearchQuery={setSearchQuery}
-          handleSearch={handleSearch}
-          handlePickRandom={handlePickRandom}
-          handleKeyDown={handleKeyDown}
-        />
-        {hasSearched && <Results searchResults={searchResults} />}
+        {isFetching ? (
+          <LoadingSpinner />
+        ) : isError ? (
+          <Alert variant="destructive">
+            <AlertTitle className="">Whoops!</AlertTitle>
+            <AlertDescription className="justify-center">
+              Something went wrong. Blame the AI and try again later!
+            </AlertDescription>
+          </Alert>
+        ) : (
+          <>
+            <SearchBar
+              searchQuery={searchQuery}
+              setSearchQuery={setSearchQuery}
+              handleSearch={handleSearch}
+              handlePickRandom={handlePickRandom}
+              handleKeyDown={handleKeyDown}
+            />
+            {searchResults && <Results searchResults={searchResults} />}
+          </>
+        )}
       </div>
     </main>
   );
