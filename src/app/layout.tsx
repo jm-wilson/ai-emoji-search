6a38e2942418c4478c19ef24f94357ee9450bb29
Joe Wilson <joe.wilson@jwils.xyz>
2025-04-18 16:16:12 -0400

    We need a toggle for dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import "@/app/globals.css"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "@/components/theme-toggle"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
+          <ModeToggle />
         </ThemeProvider>
       </body>
     </html>
