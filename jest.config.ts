ba7ae66a6e19bc6ae29e0d0b513dfbfcce3309d7
Joe Wilson <joe.wilson@jwils.xyz>
2025-04-22 15:34:27 -0400

    Set up next/jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@
  */
 
 import type {Config} from 'jest';
+import nextJest from 'next/jest';
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -90,7 +91,9 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -196,4 +199,8 @@ const config: Config = {
   // watchman: true,
 };
 
-export default config;
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+export default createJestConfig(config);
