a92ea6006539f5fad12354c3b0a50a31e324c898
Joe Wilson <joe.wilson@jwils.xyz>
2025-06-04 21:48:40 -0400

    Remove Inter (rely on tailwind for all fonts)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type React from "react"
 import "@/app/globals.css"
-import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import { ModeToggle } from "@/components/theme-toggle"
 import { Toaster } from "@/components/ui/sonner"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "EmojiFinder - Find the perfect emoji",
@@ -19,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
           <ModeToggle />
