289e5b13af2d28841a713202f1d395f341a22223
Joe Wilson <joe.wilson@jwils.xyz>
2025-12-06 13:56:12 -0500

    Yolo update all packages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    reactCompiler: true,
-  }
+  reactCompiler: true,
 };
 
 export default nextConfig;
