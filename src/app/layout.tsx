c1a789dae2392a24c61ef4786b1ac1f09cc896a8
Joe Wilson <joe.wilson@jwils.xyz>
2025-12-15 21:55:42 -0500

    Default to system theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider attribute="class" disableTransitionOnChange>
           {children}
           <ModeToggle />
           <Toaster position="bottom-center" />
